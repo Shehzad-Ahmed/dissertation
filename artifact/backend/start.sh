a014a8fc38666d2d7120b3923ba44afdc20fed12
Shehzad Ahmed <shez.ahmed187@gmail.com>
2023-09-07 22:02:30 +0100

    adds the gunicorn support to allow multi threading

--- artifact/backend/start.sh
+++ artifact/backend/start.sh
@@ -3,4 +3,6 @@
 cd app
 pip install -r requirements.txt
 python manage.py migrate
+echo yes | python manage.py collectstatic
 python manage.py runserver 0.0.0.0:8004
+#gunicorn core.wsgi:application --bind 0.0.0.0:8004
